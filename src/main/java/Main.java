db44d0594faaf705bcf6fef4aa0f539840a369a2
Tusionator <marta.kowalczyk.93@gmail.com>
2018-11-12 18:26:25 +0100

    jFuzzyLogic example implemented - it's working :D

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.*;
 
 public class Main extends Application {
 
@@ -12,6 +16,40 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+
+
+
+
+        // Load from 'FCL' file
+        //TODO change hardcode to not hartcode, or just change string - after all it's only university project,
+        // nobody will look at this code
+        String fileName = "D:\\projekty_java\\DoctorArtificial\\src\\main\\java\\tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) {
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        // Show
+        FunctionBlock functionBlock = fis.getFunctionBlock("tipper");
+        JFuzzyChart.get().chart(functionBlock);
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable tip = functionBlock.getVariable("tip");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
     }
 
 
